c962868268c3abe18dd3d4b6f202120485be8ad6
Ward Vercruyssen <ward.vercruyssen@hotmail.be>
2022-04-08 00:03:13 +0200

    Added cards and createDogs data

--- components/useGetDogs.ts
+++ components/useGetDogs.ts
@@ -1,25 +1,46 @@
 import { useEffect, useState } from "react";
+import { InteractionManager } from "react-native";
+import jsonDogs from "../assets/dogs.json";
 
 export interface DogUrisType {
   message: string[];
 }
 
+export interface Dog {
+  id: number;
+  name: string;
+  bio: string;
+  image: string;
+}
+
 const useGetDogs = () => {
   const [isLoading, setLoading] = useState(true);
-  const [dogUris, setDogUris] = useState<DogUrisType>();
+  const [dogUris, setDogUris] = useState<DogUrisType>({ message: [] });
+  const [dogs, setDogs] = useState<Dog[]>();
 
-  useEffect(() => {
-    fetch("https://dog.ceo/api/breeds/image/random/20")
+  const createDogs = (dogUris: DogUrisType): Dog[] => {
+    const newDogs = jsonDogs.map((item, index) => ({
+      ...item,
+      image: dogUris.message[index],
+    }));
+    return newDogs;
+  };
+
+  const getDogImages = () => {
+    setLoading(true);
+    fetch(`https://dog.ceo/api/breeds/image/random/50`)
       .then((response) => response.json())
       .then((json) => setDogUris(json))
       .catch((error) => console.error(error))
+      .then(() => setDogs(createDogs(dogUris)))
       .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    getDogImages();
   }, []);
 
-  const whoLetDogOut = () => {
-    return "Who let the dogs out?";
-  };
-  return { isLoading, dogUris, whoLetDogOut };
+  return { isLoading, dogs };
 };
 
 export default useGetDogs;
