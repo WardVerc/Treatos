141ef10c1e63f1d02e40d0f02af0567875f39c33
Ward Vercruyssen <ward.vercruyssen@hotmail.be>
2022-04-09 21:15:43 +0200

    refactoring + added card swipe

--- components/Dog.tsx
+++ components/Dog.tsx
@@ -1,46 +1,22 @@
-import { DogProps } from "./useGetDogs";
-import {
-  ImageBackground,
-  View,
-  Text,
-  FlatList,
-  StyleSheet,
-} from "react-native";
+import { DogType } from "./useGetDogs";
+import { ImageBackground, View, Text, StyleSheet } from "react-native";
 
-interface Props {
-  dog: DogProps;
+interface DogProps {
+  dog: DogType;
 }
 
-const Dog: React.FC<Props> = ({ dog }) => {
+const Dog: React.FC<DogProps> = ({ dog }) => {
   return (
-    <View style={styles.dogCard}>
-      <ImageBackground style={styles.dog} source={{ uri: dog.image }}>
-        <View style={styles.userInfo}>
-          <Text style={styles.name}>{dog.name}</Text>
-          <Text style={styles.bio}>{dog.bio}</Text>
-        </View>
-      </ImageBackground>
-    </View>
+    <ImageBackground style={styles.dog} source={{ uri: dog.image }}>
+      <View style={styles.userInfo}>
+        <Text style={styles.name}>{dog.name}</Text>
+        <Text style={styles.bio}>{dog.bio}</Text>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  dogCard: {
-    width: 350,
-    height: 450,
-    margin: 20,
-    marginBottom: 50,
-    borderRadius: 10,
-
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 3,
-      height: 4,
-    },
-    shadowOpacity: 0.37,
-    shadowRadius: 5.5,
-    elevation: 10,
-  },
   dog: {
     width: "100%",
     height: "100%",
